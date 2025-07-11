955f22cb2633ebfda4e0c454b4ea892f3c3fa89c
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-12 04:35:45 +0800

    Update topmodal.js

--- public/scripts/topmodal.js
+++ public/scripts/topmodal.js
@@ -1,7 +1,1 @@
-window.addEventListener("DOMContentLoaded", () => {
-  if (!localStorage.getItem("orderCafeUser")) {
-    openModal("signInModal");
-  } else {
-    showToast("Welcome back " + localStorage.getItem("orderCafeUser"));
-  }
-});
+

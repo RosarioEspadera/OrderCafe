0d33b6ffecabe59c08f96bbbce1b5c487b4b333f
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-12 04:41:16 +0800

    Update signin.js

--- public/scripts/signin.js
+++ public/scripts/signin.js
@@ -1,8 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
   if (!localStorage.getItem("orderCafeUser")) {
     openModal("signInModal");
-  } else {
-    showToast("Welcome back " + localStorage.getItem("orderCafeUser"));
   }
 });
-

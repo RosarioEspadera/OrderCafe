e755641cd624b3952fcd8154eabb2bd735710ce1
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-12 04:40:18 +0800

    Update topmodal.js

--- public/scripts/topmodal.js
+++ public/scripts/topmodal.js
@@ -1,1 +1,10 @@
+function openModal(id) {
+  document.getElementById(id)?.classList.add("visible");
+  document.querySelector(".modal-backdrop")?.classList.add("visible");
+}
+
+function closeModal(id) {
+  document.getElementById(id)?.classList.remove("visible");
+  document.querySelector(".modal-backdrop")?.classList.remove("visible");
+}
 

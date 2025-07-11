f02530e287e637531a43b64b9abd91b6623a0edc
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-07-12 07:37:43 +0800

    Update receipt.js

--- public/scripts/receipt.js
+++ public/scripts/receipt.js
@@ -1,9 +1,11 @@
-document.getElementById("confirmSend").addEventListener("click", () => {
-  emailjs.send("service_epydqmi", "template_vzuexod", {
-    to_name: "Rosario",
-    from_name: "OrderCafe",
-    message: "Your order has been confirmed!",
-    reply_to: "rosario@ordercafe.com"
+const confirmButton = document.getElementById("confirmSend");
+if (confirmButton) {
+  confirmButton.addEventListener("click", () => {
+    emailjs.send("service_epydqmi", "template_vzuexod", {
+      to_name: "Rosario",
+      from_name: "OrderCafe",
+      message: "Your order has been confirmed!",
+      reply_to: "rosario@ordercafe.com"
+    });
   });
-});
-
+}
